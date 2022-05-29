47b05b75ad04e15540828edb358dfa66ff953b6d
Trelel129 <89815856+Trelel129@users.noreply.github.com>
2022-05-29 21:43:14 +0700

    Update A_5025201214.r

--- A_5025201214.r
+++ A_5025201214.r
@@ -59,3 +59,83 @@
     
   # 3F
     # Hipotesis Awal (H0), yaitu "Rata-rata saham di bandung sama dengan di Bali" diterima
+
+
+# ============================ Soal 4 ===================================
+dataoneway <- read.table("onewayanova.txt",h=T)
+attach(dataoneway)
+names(dataoneway)
+
+dataoneway$Group <- as.factor(dataoneway$Group)
+dataoneway$Group = factor(dataoneway$Group,labels = c("Grup 1", "Grup 2", "Grup 3"))
+
+class(dataoneway$Group)
+
+#a) Pembagian menjadi 3 subjek grup dan membuat
+# plot kuantil normal setiap kelompok
+
+Group1 <- subset(dataoneway, Group == "Grup 1")
+Group2 <- subset(dataoneway, Group == "Grup 2")
+Group3 <- subset(dataoneway, Group == "Grup 3")
+
+qqnorm(Group1$Length)
+qqline(Group1$Length)
+
+qqnorm(Group2$Length)
+qqline(Group2$Length)
+
+qqnorm(Group2$Length)
+qqline(Group2$Length)
+
+#b) Mencari homogenity of variances
+bartlett.test(Length ~ Group, data = dataoneway)
+
+#One Way ANOVA - Test if the means of the k populations are equal
+#c) Uji anova satu arah
+model1 = lm(Length ~ Group, data = dataoneway)
+anova(model1)
+
+#d) nilai p adalah 0.8054, maka H0 ditolak
+
+#e) Post-hoc test Tukey HSD
+TukeyHSD(aov(model1))
+# hasil dari test post-hoc model 1 adalah grup 1 lebih panjang
+# dari grup yang lain
+#Data visualisation
+
+# ============================ Soal 5 ===================================
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+#a) plot sederhana
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+
+#variabel untuk anova
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+#b) uji anova dua arah
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+summary(anova)
+
+#c) tabel dengan mean dan standar deviasi keluaran cahaya
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+# Tukey's test
+print("Uji Tukey:")
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+# compact letter display
+print("Compact Letter Display:")
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
